ffd5de7c3e3a9cd0ae1f03c4383548cb6699cae9
Yuta Koshizawa <koher@koherent.org>
2018-02-10 02:31:42 +0900

    Support Carthage

---
+++ Sources/AsyncK/AsyncK.h
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for AsyncK.
+FOUNDATION_EXPORT double AsyncKVersionNumber;
+
+//! Project version string for AsyncK.
+FOUNDATION_EXPORT const unsigned char AsyncKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AsyncK/PublicHeader.h>
+
+
