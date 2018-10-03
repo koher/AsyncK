c3adff656a21a5eda2f9f220d470bfdd6c6a0530
Yuta Koshizawa <koher@koherent.org>
2018-10-03 17:55:12 +0900

    Implement `suspendAsync(...) -> Async<Throws<T>>`

--- Sources/AsyncK/Functions.swift
+++ Sources/AsyncK/Functions.swift
@@ -2,6 +2,21 @@ public func beginAsync(_ body: () -> Async<Void>) {
     _ = body()
 }
 
-public func suspendAsync<T>(_ body: (_ continuation: @escaping (T) -> ()) -> ()) -> Async<T> {
+public func suspendAsync<T>(
+    _ body: (_ continuation: @escaping (T) -> ()) -> ()
+) -> Async<T> {
     return Async<T>(body)
+}
+
+public func suspendAsync<T>(
+    _ body: (_ continuation: @escaping (T) -> (),
+             _ error: @escaping (Error) -> ()) -> ()
+) -> Async<Throws<T>> {
+    return Async<Throws<T>> { continuation in
+        body({ value in
+            continuation(Throws(value: value))
+        }, { error in
+            continuation(Throws(error: error))
+        })
+    }
 }
