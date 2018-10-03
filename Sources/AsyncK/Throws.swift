55099dfb2ea07666a84b6670b9090c83b071fc50
Yuta Koshizawa <koher@koherent.org>
2018-10-03 18:30:29 +0900

    Separate `tryAwait` from Throws.swift

--- Sources/AsyncK/Throws.swift
+++ Sources/AsyncK/Throws.swift
@@ -30,58 +30,6 @@ extension Throws {
     }
 }
 
-extension Async {
-    public func tryAwait<T, V>(_ operation: @escaping (V) throws -> T) -> Async<Throws<T>> where Value == Throws<V> {
-        return Async<Throws<T>> { continuation in
-            get { throwsValue in
-                let newThrowsValue: Throws<T>
-                do {
-                    let newValue = try operation(try throwsValue.get())
-                    newThrowsValue = Throws(value: newValue)
-                } catch let error {
-                    newThrowsValue = Throws(error: error)
-                }
-                continuation(newThrowsValue)
-            }
-        }
-    }
-    
-    public func tryAwait<T, V>(_ operation: @escaping (V) throws -> Async<T>) -> Async<Throws<T>> where Value == Throws<V> {
-        return Async<Throws<T>> { continuation in
-            get { throwsValue in
-                do {
-                    let value = try throwsValue.get()
-                    
-                    try operation(value).get { newValue in
-                        let newThrowsValue = Throws<T>(value: newValue)
-                        continuation(newThrowsValue)
-                    }
-                } catch let error {
-                    let newThrowsValue = Throws<T>(error: error)
-                    continuation(newThrowsValue)
-                }
-            }
-        }
-    }
-    
-    public func tryAwait<T, V>(_ operation: @escaping (V) throws -> Async<Throws<T>>) -> Async<Throws<T>> where Value == Throws<V> {
-        return Async<Throws<T>> { continuation in
-            get { throwsValue in
-                do {
-                    let value = try throwsValue.get()
-                    
-                    try operation(value).get { newThrowsValue in
-                        continuation(newThrowsValue)
-                    }
-                } catch let error {
-                    let newThrowsValue = Throws<T>(error: error)
-                    continuation(newThrowsValue)
-                }
-            }
-        }
-    }
-}
-
 fileprivate enum Result<Value> {
     case success(Value)
     case failure(Error)
